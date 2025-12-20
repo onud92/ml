ae75281bd00c99eb8144f9f5f637e3b2b77470d1
onud92 <onur.de@live.de>
2025-12-20 09:45:18 +0100

    erstes eigenes modul eingebunden für einfache textausgabe

---
+++ modules/ml_hello/ml_hello.c
@@ -0,0 +1,10 @@
+#include <zephyr/kernel.h>
+
+#include "ml_hello.h"
+
+void ml_hello(void)
+{
+    printk("\r\n ------------------------------------------ \r\n");
+    printk("\r\n A machine learning application by onud92.. \r\n");
+    printk("\r\n ------------------------------------------ \r\n");
+}
---
+++ modules/ml_hello/ml_hello.h
@@ -0,0 +1,7 @@
+#ifndef ML_HELLO_H
+#define ML_HELLO_H
+
+/* Public function decleration */
+void ml_hello(void);
+
+#endif /* ML_HELLO_H */
