e7b7af44eb9c72c97686ea10dfd59879fed33f8b
onud92 <onur.de@live.de>
2025-12-23 12:07:54 +0100

    create docker for python ml algorithms

--- modules/ml_alg/README.md
+++ modules/ml_alg/README.md
@@ -1,4 +1,38 @@
-``shell
-# Für die installation für tensorflow und cuda für die GPU
-pip install "tensorflow[and-cuda]"
+## Installation von Docker
+
+```shell
+# 1. Paketliste aktualisieren
+sudo apt update
+
+# 2. Docker und Hilfsprogramme installieren
+sudo apt install -y docker.io curl
+
+# 3. Docker-Dienst starten und aktivieren (damit er beim Booten angeht)
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Ihren Benutzer zur Gruppe 'docker' hinzufügen und PC neu starten
+sudo usermod -aG docker $USER
+
+# repo hinzufügen
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+# installieren und konfigurieren
+sudo apt update
+sudo apt install -y nvidia-container-toolkit
+
+# Sagt Docker, dass es NVIDIA nutzen soll
+sudo nvidia-ctk runtime configure --runtime=docker
+
+# Startet Docker neu, um die Änderungen zu übernehmen
+sudo systemctl restart docker
+
+# Image bauen
+docker build -t my_image .
+
+# run 
+docker run --rm --gpus all -v $(pwd):/workspace -w /workspace my_image python hello_tf.py
 ```
--- modules/ml_alg/hello_tf.py
+++ modules/ml_alg/hello_tf.py
@@ -1,5 +1,7 @@
 # use tensorflow library
 import tensorflow as tf
+# Keras is TensorFlow's high-level API for deep learning
+from tensorflow import keras
 # use math library numpy
 import numpy as np
 # use graphing library Matplotlib
@@ -7,7 +9,15 @@ import matplotlib.pyplot as plt
 # use Python's math library
 import math
 
-
+# Check, ob GPU gesehen wird
+print("------------------------------------------------")
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    print(f"ERFOLG! Folgende GPU wurde gefunden: {gpus}")
+    print("RTX 5060 Power wird genutzt!")
+else:
+    print("WARNUNG: Keine GPU gefunden. Läuft auf CPU.")
+print("------------------------------------------------")
 
 # generate this many sample datapoints
 SAMPLES = 1000
@@ -42,9 +52,23 @@ y_train, y_validate, y_test = np.split(y_values, [TRAIN_SPLIT, TEST_SPLIT])
 # Double check that our splits add up correctly
 assert (x_train.size + x_validate.size + x_test.size) == SAMPLES
 
+# We'll use Keras to create a simple model architecture
+model_1 = tf.keras.Sequential()
+
+# First layer takes a scalar input and feeds it through 8 "neurons". The
+# neurons decide whether to activate based on the 'relu' activation function.
+model_1.add(keras.layers.Dense(8, activation='relu', input_shape=(1,)))
+# Final layer is a single neuron, since we want to output a single value
+model_1.add(keras.layers.Dense(1))
+# Compile the model using the standard 'adam' optimizer and the mean squared error or 'mse' loss function for regression.
+model_1.compile(optimizer='adam', loss='mse', metrics=['mae'])
+
+model_1.summary()
+
 # Plot the data in each partition in different colors:
 plt.plot(x_train, y_train, 'b.', label="Train")
 plt.plot(x_validate, y_validate, 'y.', label="Validate")
 plt.plot(x_test, y_test, 'r.', label="Test")
 plt.legend()
-plt.show()
+plt.savefig("pics/split_data_sinus.png") 
+print("Grafik wurde als 'ergebnis_grafik.png' gespeichert!")
