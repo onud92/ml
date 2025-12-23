a5fa019641e5694a48efca3852a7846933218308
onud92 <onur.de@live.de>
2025-12-23 09:28:44 +0100

    split data for training, test and validation

--- modules/ml_alg/hello_tf.py
+++ modules/ml_alg/hello_tf.py
@@ -31,6 +31,17 @@ y_values = np.sin(x_values)
 # Add a small random number to each y value
 y_values += 0.1 * np.random.randn(*y_values.shape)
 
+# use 60% of data for training, 20% for testing and 20% for validation
+TRAIN_SPLIT = int(0.6 * SAMPLES)
+TEST_SPLIT = int(0.2 * SAMPLES + TRAIN_SPLIT)
+
+# chop data into three parts
+x_train, x_validate, x_test = np.split(x_values, [TRAIN_SPLIT, TEST_SPLIT])
+y_train, y_validate, y_test = np.split(y_values, [TRAIN_SPLIT, TEST_SPLIT])
+
+# Double check that our splits add up correctly
+assert (x_train.size + x_validate.size + x_test.size) == SAMPLES
+
 # Plot our data. The 'b.' argument tells the library to print blue dots.
 plt.plot(x_values, y_values, 'b.')
 plt.show()
